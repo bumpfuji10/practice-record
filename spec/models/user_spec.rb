f426255182dde8f6348c8ea2d1365e89f78ef358
Yo Kamada <bumpfuji10@gmail.com>
2024-03-26 20:31:57 +0900

    setup: rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe "test" do
+
+    context "test" do
+      it "test" do
+        pp "test"
+      end
+    end
+  end
+end
