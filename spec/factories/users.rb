783789f6826881cae4550e37a4cc1d3638f55d14
Yo Kamada <bumpfuji10@gmail.com>
2024-03-26 23:38:32 +0900

    add: rspec about password validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     name { "Yo Kamada" }
     email { "test@test.com" }
+    password { "123456" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  describe "validation" do
+  describe "validations" do
 
     context "nameとemailが存在する場合" do
       let!(:user) { FactoryBot.create(:user, name: "Yo Kamada", email: "test@test.com") }
@@ -30,6 +30,38 @@ RSpec.describe User, type: :model do
 
     context "emailが正規表現に則っていない場合" do
       let!(:user) { FactoryBot.build(:user, email: "test@mail") }
+
+      it "Userが作成されないこと" do
+        expect(user).to be_invalid
+      end
+    end
+
+    context "passwordが存在している場合" do
+      let!(:user) { FactoryBot.build(:user, password: "hogehoge") }
+
+      it "Userが作成されること" do
+        expect(user).to be_valid
+      end
+    end
+
+    context "passwordが存在しない場合" do
+      let!(:user) { FactoryBot.build(:user, password: nil) }
+
+      it "Userが作成されないこと" do
+        expect(user).to be_invalid
+      end
+    end
+
+    context "passwordの長さが6文字未満の場合" do
+      let!(:user) { FactoryBot.build(:user, password: "12345") }
+
+      it "Userが作成されないこと" do
+        expect(user).to be_invalid
+      end
+    end
+
+    context "passwordの長さが12文字より多い場合" do
+      let!(:user) { FactoryBot.build(:user, password: "1234567890123") }
 
       it "Userが作成されないこと" do
         expect(user).to be_invalid
