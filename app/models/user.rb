c752c5e1b96321bf32b933cbdcbfd578b927b9cc
Yo Kamada <bumpfuji10@gmail.com>
2024-03-26 23:25:44 +0900

    add: has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
 end
