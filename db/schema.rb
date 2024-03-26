2c977ab196ecfe8dcb0307080f47dd988a71d35c
Yo Kamada <bumpfuji10@gmail.com>
2024-03-26 23:16:11 +0900

    add: column password_digest users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_26_111910) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_26_141456) do
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
   end
