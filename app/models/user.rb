c49cd42307606a2e75a7498e771a9f44f8e552a9
Yo Kamada <bumpfuji10@gmail.com>
2024-03-31 00:05:47 +0900

    add: users migration

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  validates: :name, presence: true
+  validates: :email, presence: true, uniqueness: true, format: { with: "/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i" }
+  validates: :password, presence: true, length: { in: 6..12 }
+  has_secure_password
+end
