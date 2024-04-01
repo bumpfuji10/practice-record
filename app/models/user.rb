0c70033ed0c5df4ed2c0cb4f1738d65eeb08d8e7
Yo Kamada <bumpfuji10@gmail.com>
2024-04-02 07:39:10 +0900

    add migration password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
                     uniqueness: true,
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
 end
