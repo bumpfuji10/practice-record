4bb49c62446e04fdd9923eb243a217738ee49efb
Yo Kamada <bumpfuji10@gmail.com>
2024-04-01 19:46:43 +0900

    migration users

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    length: { in: 6..255 }
+end
