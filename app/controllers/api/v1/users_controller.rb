3c3219edbc1841e3022363d98126d37f9863ce04
Yo Kamada <bumpfuji10@gmail.com>
2024-04-07 19:37:29 +0900

    ベーシックなrequest specを作成

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,15 @@
 class Api::V1::UsersController < ApplicationController
-
   def index
     users = User.all
-    render(json: users)
+    render json: users
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render(json: user, status: :created)
+      render json: user, status: :created
     else
-      render(json: { errors: user.errors, full_message: user.errors.full_messages }, status: unprocessable_entity)
+      render json: { errors: user.errors, full_message: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
