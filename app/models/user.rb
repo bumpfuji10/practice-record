a2d74670e4445f87d50af5c031d346450beeaa93
Yo Kamada <bumpfuji10@gmail.com>
2024-04-14 18:23:54 +0900

    add: activateしたユーザーのtokenの有効期限と、ユーザーの情報を返却するcontrollerを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,19 @@ class User < ApplicationRecord
   validates :email, presence: true,
                     uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
   has_secure_password
+
+  def self.find_activated(email)
+    find_by(email: email, activated: true)
+  end
+
+  def email_activate?
+    users = User.where.not(id: id)
+    users.find_activated(email).present?
+  end
+
+  def my_json
+    as_json(only: [:id, :name, :email, :created_at])
+  end
 end
